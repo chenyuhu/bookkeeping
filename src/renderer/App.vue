d09b23d3c1ca086f0f61402f3afae81ffe9996f5
chenfeng <chenfeng@sishuxuefu.com>
2019-09-11 23:37:03 +0800

    构建项目

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Bookkeeping'
+}
+</script>
+
+<style>
+/* CSS */
+</style>
